fa9e69afeabcf10163c7a09379b17449d77bd9c0
Tranhau5574 <tranvanhaubg2003@gmail.com>
2024-05-23 01:29:40 +0700

    Fix Header

--- Front_end/src/components/SignOutButton.tsx
+++ Front_end/src/components/SignOutButton.tsx
@@ -23,7 +23,8 @@ const SignOutButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-100 "
+      className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    
     >
       Sign Out
     </button>
