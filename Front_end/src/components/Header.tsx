5d7de69af6d6cd64973a8d7667f7f542a6fa02b4
ngominhhieu2603 <2603ngominhhieu@gmail.com>
2024-05-22 16:01:50 +0700

    addHotel

--- Front_end/src/components/Header.tsx
+++ Front_end/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-// import { useAppContext } from "../contexts/AppContext";
-// import SignOutButton from "./SignOutButton";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
 
 const Header = () => {
-  //   const { isLoggedIn } = useAppContext();
-
+  const { isLoggedIn } = useAppContext();
+  console.log(isLoggedIn);
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
@@ -12,13 +12,30 @@ const Header = () => {
           <Link to="/">SmartBooking.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/login"
-            className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
-          >
-            Login
-          </Link>
-          <Link to = "/register" className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"> 33Sigeenup22 </Link>
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                to="/my-hotels"
+              >
+                My Hotels
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              to="/login"
+              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            >
+              Login
+            </Link>
+          )}
         </span>
       </div>
     </div>
