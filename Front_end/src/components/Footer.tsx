334b591b7dbe2f091ac6273d5c22d28d1a70c047
Tranhau5574 <tranvanhaubg2003@gmail.com>
2024-05-22 17:36:47 +0700

    Fix Header

--- Front_end/src/components/Footer.tsx
+++ Front_end/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="bg-white dark: bg-slate-500">
+      <footer className="bg-blue-600 dark: bg-slate-500">
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
